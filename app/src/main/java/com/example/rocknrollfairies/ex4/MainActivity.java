a8877f4292ea2b302ecfe76b3dc4cfa5123c80a8
Shira Negbi <shira.negbi@live.biu.ac.il>
2019-06-19 21:48:03 +0300

    Make the "Connect" button lead to the second activity. Draw the joystick and start implementing its movement.

--- app/src/main/java/com/example/rocknrollfairies/ex4/MainActivity.java
+++ app/src/main/java/com/example/rocknrollfairies/ex4/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rocknrollfairies.ex4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void connect(View view) {
+        //EditText editText = (EditText)findViewById(R.id.editText);
+        Intent intent = new Intent(this, Joystick.class);
+        //String msg = editText.getText().toString();
+        //intent.putExtra("message", msg);
+        startActivity(intent);
     }
 }
